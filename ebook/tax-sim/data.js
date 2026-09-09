edc3e195bc0342a8bb7b0e7811dcc1a6cecd8cd7
nishikireo <62166194+nishikireo@users.noreply.github.com>
2026-09-10 02:40:37 +0900

    eBook一覧用メタデータと発行日形式を整備

--- ebook/tax-sim/data.js
+++ ebook/tax-sim/data.js
@@ -7,7 +7,9 @@ window.bookMarkdown = `
 ---
 title: 節税のトリセツ！あなたの節税効果が一目でわかる！！
 cover: img/cover.webp
-published: 2026年 2月 1日
+published: 2026-02-01
+description: 不動産投資による節税効果を、年収との関係も含めてわかりやすく解説。
+themes: 節税
 ---
 # 所得税の考え方とは！？
 ![](img/Setsuzei_tori_img_1.webp)
