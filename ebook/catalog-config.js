9ccced2cd5df264768e46812d9fff5cb898178fb
nishikireo <62166194+nishikireo@users.noreply.github.com>
2026-09-10 07:19:06 +0900

    動画セミナー設定をカタログに追加

--- ebook/catalog-config.js
+++ ebook/catalog-config.js
@@ -1,7 +1,7 @@
 (() => {
     "use strict";
 
-    window.EBOOK_CATALOG_CONFIG = Object.freeze({
+    window.LIBRARY_CATALOG_CONFIG = Object.freeze({
         books: [
             "land2026",
             "tax-sim",
@@ -16,12 +16,60 @@
             "nisa",
             "ownersvoice"
         ],
+
         themes: [
             "不動産投資",
             "節税",
             "資産形成",
             "市況",
             "オーナー事例"
+        ],
+
+        videos: [
+            {
+                title: "動画セミナー 01",
+                url: "https://www.youtube.com/watch?v=vWo8G20qW18&feature=youtu.be"
+            },
+            {
+                title: "動画セミナー 02",
+                url: "https://m.youtube.com/watch?v=qL4tXyGl3fY"
+            },
+            {
+                title: "動画セミナー 03",
+                url: "https://www.youtube.com/watch?v=4sm2HB3koH8"
+            },
+            {
+                title: "動画セミナー 04",
+                url: "https://www.youtube.com/watch?si=tHtYwI8C8pwKf_1v&v=m8tmkhCwL5E&feature=youtu.be"
+            },
+            {
+                title: "動画セミナー 05",
+                url: ""
+            },
+            {
+                title: "動画セミナー 06",
+                url: ""
+            },
+            {
+                title: "動画セミナー 07",
+                url: ""
+            },
+            {
+                title: "動画セミナー 08",
+                url: ""
+            },
+            {
+                title: "動画セミナー 09",
+                url: ""
+            },
+            {
+                title: "動画セミナー 10",
+                url: ""
+            },
+            {
+                title: "動画セミナー 11",
+                url: ""
+            }
         ]
     });
 })();
