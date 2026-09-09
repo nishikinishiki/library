fcc1ae9d8d68d81e50cbb537874f713dbd570df0
nishikireo <62166194+nishikireo@users.noreply.github.com>
2026-09-10 02:40:07 +0900

    eBook一覧の公開順とテーマ設定を追加

---
+++ ebook/catalog-config.js
@@ -0,0 +1,27 @@
+(() => {
+    "use strict";
+
+    window.EBOOK_CATALOG_CONFIG = Object.freeze({
+        books: [
+            "land2026",
+            "tax-sim",
+            "renovation",
+            "osaka",
+            "tax-realestate",
+            "tax-ebook",
+            "3ways",
+            "invest-ebook",
+            "land2025",
+            "millionaire",
+            "nisa",
+            "ownersvoice"
+        ],
+        themes: [
+            "不動産投資",
+            "節税",
+            "資産形成",
+            "市況",
+            "オーナー事例"
+        ]
+    });
+})();
