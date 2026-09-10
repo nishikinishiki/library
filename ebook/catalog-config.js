dc74b421599ccdbb58b7e219a724688d223f53bc
nishikinishiki <r-nishiki@jpreturns.com>
2026-09-10 09:45:52 +0900

    Refactor code structure for improved readability and maintainability

--- ebook/catalog-config.js
+++ ebook/catalog-config.js
@@ -31,7 +31,7 @@
                 duration: "約18分",
                 lecturer: "稲垣 創太",
                 description: "【「不動産投資」について徹底解説！】の後編をお届け。後編では、不動産投資の覚えておきたい「6つのメリット」と気を付けるべき「6つのリスク」を解説。",
-                url: "https://www.youtube.com/watch?v=vWo8G20qW18&feature=youtu.be"
+                url: "https://www.youtube.com/watch?v=vWo8G20qW18"
             },
             {
                 title: "【Dr's Prime対談】節税シミュレーション徹底解説！",
@@ -52,56 +52,56 @@
                 duration: "約40分",
                 lecturer: "J.P.ReturnsオーナーS様×室田雄飛",
                 description: "家賃上昇ってどうなるの？これから都心の不動産投資はどう考えるべき？そんなギモンに、業界歴20年以上のプロが解説。最新不動産市況を徹底解説！",
-                url: "https://www.youtube.com/watch?si=tHtYwI8C8pwKf_1v&v=m8tmkhCwL5E&feature=youtu.be"
+                url: "https://www.youtube.com/watch?v=Fxq90bV7RFU"
             },
             {
                 title: "【利益爆増】ワンルームの虎が教える出口戦略・勝ちたかったらメルカリよりブックオフで売れ！",
                 duration: "約20分",
                 lecturer: "【コラボ】マッキーとワンルーム投資チャンネル×染谷重幸",
                 description: "ワンルームマンション投資で利益を増やすカギは、「買い方」だけでなく出口戦略（売り方）も重要！業界トップクラスの営業マンから利益を最大化する考え方や実践的な売却戦略を徹底解説。",
-                url: ""
+                url: "https://www.youtube.com/watch?v=4sm2HB3koH8"
             },
             {
                 title: "【買い方・売り方】年収1000万超えたら絶対必見！トップ営業マンが教える…“成功の方程式”",
                 duration: "約14分",
                 lecturer: "【コラボ】マッキーとワンルーム投資チャンネル×染谷重幸",
                 description: "“高属性がワンルーム投資で成功するための方程式”を徹底解説。高収入だからこそ気づきにくい落とし穴や、正しい物件の選び方、無理のない資金計画の組み立て方、そして出口戦略の考え方まで――。",
-                url: ""
+                url: "https://www.youtube.com/watch?v=m8tmkhCwL5E"
             },
             {
                 title: "【Dr's Prime対談】医師のための節税対策＋資産形成",
                 duration: "約23分",
                 lecturer: "【コラボ】Dr's Prime × 染谷 重幸",
                 description: "医師向けメディアを運営しているDr's Primeとの対談動画。Dr's Prime代表であり、医師である田様が面白いと語った不動産投資の魅力とは？",
-                url: ""
+                url: "https://www.youtube.com/watch?v=qL4tXyGl3fY"
             },
             {
                 title: "節税セミナー",
                 duration: "約25分",
                 lecturer: "染谷 重幸",
                 description: "節税の仕組み・選択肢・年収別の税効果をわかりやすく解説。生命保険控除、確定拠出年金、ふるさと納税、不動産投資についてそれぞれの特徴を約25分間に凝縮したマネーセミナー。",
-                url: ""
+                url: "https://www.youtube.com/watch?v=9a0Izi3LUsk"
             },
             {
                 title: "不動産投資【徹底解説】",
                 duration: "約18分",
                 lecturer: "稲垣 創太",
                 description: "「不動産投資」について徹底解説！現役トップセールスマンが登壇し、知りたいポイントをまとめてご紹介。初心者はもちろん、経験者の方も知らない知識が得られるかもしれません。こちらの動画では前編をお届け！",
-                url: ""
+                url: "https://www.youtube.com/watch?v=NFuumOR-aXE"
             },
             {
                 title: "資産形成セミナー",
                 duration: "約25分",
                 lecturer: "染谷 重幸",
                 description: "資産運用と資産形成についての考え方や不動産投資のコツなど初心者から経験者まで学びやすい内容をコンパクトにまとめてお届け。投資の勉強法についてもご紹介。",
-                url: ""
+                url: "https://www.youtube.com/watch?v=Bf83X7tSqo0"
             },
             {
                 title: "医師向け資産形成セミナー",
                 duration: "約17分",
                 lecturer: "染谷 重幸",
                 description: "今回のテーマは、「医師向けの資産形成」。医師として支払う税金のことや堅実な資産形成など、失敗しないための戦略と戦術をご紹介。",
-                url: ""
+                url: "https://www.youtube.com/watch?v=dFriIedkWuE"
             }
         ]
     });
